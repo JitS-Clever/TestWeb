cec7f7ddb7fa165ba7f766aa0c125d6f9cf1fc6c
Jitendra <jitendra.sahoo@clevertap.com>
2025-07-15 10:07:40 +0530

    Dynamic Home Page via Web PE

--- js/jbuy.js
+++ js/jbuy.js
@@ -1,20 +1,37 @@
 var pageDict = { PageName: "JBuy Home" };
-var PIPVideo;
+var PIPVideo, JBuy_Home;
+
+function homeReorder(JBuyHome) {
+    const container = document.getElementById("mainContainer");
+
+    // Convert sectionOrder to an array of [id, position] and sort by position
+    const orderedSections = Object.entries(JBuyHome)
+        .sort((a, b) => a[1] - b[1]) // Sort by position value
+        .map(([id]) => document.getElementById(id)); // Get DOM elements
+    console.log(orderedSections);
+    // Append elements in new order
+    orderedSections.forEach(section => {
+        if (section) {
+            container.appendChild(section); // Re-appends (moves) section in order
+        }
+    });
+
+}
 
 function defineVariable() {
 
     PIPVideo = clevertap.defineVariable("PIPVideo", "{\"enabled\":false,\"video_url\":\"NA\",\"position\":\"NA\",\"redirect_url\":\"NA\"}");
     BulletNudge = clevertap.defineVariable("BulletNudge", "{\"enabled\":false,\"title\":\"EOSSale\",\"time\":\"\",\"redirect_url\":\"https://jits-clever.github.io/TestWeb/\",\"bg_colour\":\"\",\"icon_url\":\"\",\"title_color\":\"\",\"timer_color\":\"\"}");
-
+    JBuy_Home = clevertap.defineVariable("JBuy_Home", "{\"heroCarousel\": 1,\"specialOffer\": 2,\"topCategories\": 3,\"topProducts\": 4,\"featuredBrands\": 5}");
 
 }
 
 function raisePageView(pageDict) {
-    var evProperties = { "Page Name": pageDict["PageName"], "source":pageDict["source"],"AppVersion":pageDict["appVersion"]}
+    var evProperties = { "Page Name": pageDict["PageName"], "source": pageDict["source"], "AppVersion": pageDict["appVersion"] }
     if (window.CleverTap) {
-        CleverTap.pushEvent("Page Viewed",JSON.stringify(evProperties));
+        CleverTap.pushEvent("Page Viewed", JSON.stringify(evProperties));
     } else {
-        clevertap.event.push("Page Viewed",evProperties)
+        clevertap.event.push("Page Viewed", evProperties)
     }
 
 }
@@ -45,27 +62,21 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+    clevertap.setLogLevel(4);
     raisePageView(pageDict);
 
     defineVariable();
+    clevertap.syncVariables(
+        // Success callback function  
+        () => { console.log('Sync successful'); },
+
+        // Failure callback function  
+        () => { console.log('Sync failed'); }
+    );
     clevertap.fetchVariables(
         () => {
             console.log("Fetch successful");
-            // if (PIPVideo) {
-            //     const PIPvalue = PIPVideo.getValue();
-            //     const NudgeValue = BulletNudge.getValue();
-
-            //     try {
-            //         if (window.jBuyBridge && jBuyBridge.triggerPiP && jBuyBridge.triggerBullet) {
-            //             jBuyBridge.triggerPiP(PIPvalue);
-            //             jBuyBridge.triggerBullet(NudgeValue);
-            //         } else {
-
-            //         }
-            //     } catch (err) {
-            //         console.error("Error parsing PIPVideo value:", err);
-            //     }
-            // }
+            homeReorder(JSON.parse(JBuy_Home.getValue()));
         }
     );
 
